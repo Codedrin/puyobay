33a3aa33d92df4fc373e6df079b85a3f37d4e067
Aldrin Rosales <104087976+Codedrin@users.noreply.github.com>
2024-11-06 19:59:21 +0800

    Update LandlordAddProperty.jsx

--- client/src/views/pages/landlord/LandlordAddProperty.jsx
+++ client/src/views/pages/landlord/LandlordAddProperty.jsx
@@ -111,7 +111,7 @@ const LandlordAddProperty = () => {
         userId: userId,
       };
   
-      await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/users/add-property/${userId}`, propertyData);
+      await axios.post(`http://localhost:5000/api/users/add-property/${userId}`, propertyData);
   
       toast.success('Property added successfully');
       setProperty({
@@ -139,186 +139,96 @@ const LandlordAddProperty = () => {
   
   return (
     <div>
-      <LandlordNavbar />
-
-      <div className="container mx-auto p-4">
-        <ToastContainer />
-        
-        {/* Add New Property Section */}
-        <div>
-          <h2 className="text-2xl font-semibold mb-4">Add New Property</h2>
-          <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            
-            <div className="mb-4">
-              <label className="block text-gray-700">Property Name</label>
+    <LandlordNavbar />
+    <div className="container mx-auto p-4">
+      <ToastContainer />
+      
+      <div>
+        <h2 className="text-2xl font-semibold mb-4">Add New Property</h2>
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          
+          {['propertyName', 'description', 'address', 'price', 'availableRooms', 'area', 'longitude', 'latitude'].map((field, index) => (
+            <div className="mb-4" key={index}>
+              <label className="block text-gray-700 capitalize">{field.replace(/([A-Z])/g, ' $1')}</label>
               <input
                 type="text"
-                name="propertyName"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.propertyName}
+                name={field}
+                className="w-full px-3 py-2 border rounded-lg focus:outline-none"
+                value={property[field]}
                 onChange={handleChange}
                 required
               />
             </div>
+          ))}
 
-            <div className="mb-4">
-              <label className="block text-gray-700">Description</label>
-              <textarea
-                name="description"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.description}
-                onChange={handleChange}
-                required
-              ></textarea>
-            </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Select Area:</label>
+            <select
+              name="propertyArea"
+              className="w-full px-3 py-2 border rounded-lg"
+              value={property.propertyArea}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Select Area</option>
+              <option value="San Isidro">San Isidro</option>
+              <option value="Del Carmen">Del Carmen</option>
+            </select>
+          </div>
 
-            <div className="mb-4">
-              <label className="block text-gray-700">Address</label>
-              <input
-                type="text"
-                name="address"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.address}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            
-            {/* Area Selection Dropdown */}
-            <div className="mb-4">
-              <label className="block text-gray-700">Select Area:</label>
-              <select
-                name="propertyArea"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.propertyArea}
-                onChange={handleChange}
-                required
-              >
-                <option value="">Select Area</option>
-                <option value="San Isidro">San Isidro</option>
-                <option value="Del Carmen">Del Carmen</option>
-              </select>
-            </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Type:</label>
+            <select
+              name="propertyType"
+              className="w-full px-3 py-2 border rounded-lg"
+              value={property.propertyType}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Select Type</option>
+              <option value="House">House</option>
+              <option value="Apartment">Apartment</option>
+            </select>
+          </div>
 
-            {/* Type Selection Dropdown */}
-            <div className="mb-4">
-              <label className="block text-gray-700">Type:</label>
-              <select
-                name="propertyType"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.propertyType}
-                onChange={handleChange}
-                required
-              >
-                <option value="">Select Type</option>
-                <option value="House">House</option>
-                <option value="Apartment">Apartment</option>
-              </select>
-            </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Upload Images</label>
+            <input
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              className="w-full px-3 py-2 border rounded-lg"
+              required
+            />
+          </div>
 
-            <div className="mb-4">
-              <label className="block text-gray-700">Price</label>
-              <input
-                type="text"
-                name="price"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.price}
-                onChange={handleChange}
-                required
-              />
-            </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Upload GCash QR Code</label>
+            <input
+              type="file"
+              onChange={handleGcashQrChange}
+              className="w-full px-3 py-2 border rounded-lg"
+            />
+          </div>
 
-            <div className="mb-4">
-              <label className="block text-gray-700">Available Rooms</label>
-              <input
-                type="text"
-                name="availableRooms"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.availableRooms}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700">Longitude</label>
-              <input
-                type="text"
-                name="longitude"
-                placeholder="125.96840777495305"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.longitude}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700">Latitude</label>
-              <input
-                type="text"
-                name="latitude"
-                placeholder="9.871556045663597"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.latitude}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700">Room Area</label>
-              <input
-                type="text"
-                name="area"
-                className="w-full px-3 py-2 border rounded-lg"
-                value={property.area}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            {/* Upload Images */}
-            <div className="mb-4">
-              <label className="block text-gray-700">Upload Images</label>
-              <input
-                type="file"
-                multiple
-                onChange={handleFileChange}
-                className="w-full px-3 py-2 border rounded-lg"
-                required
-              />
-            </div>
-
-            {/* GCash QR Code */}
-            <div className="mb-4">
-              <label className="block text-gray-700">Upload GCash QR Code</label>
-              <input
-                type="file"
-                onChange={handleGcashQrChange}
-                className="w-full px-3 py-2 border rounded-lg"
-              />
-            </div>
-
-            <div className="flex justify-end col-span-2">
-              <button
-                type="submit"
-                className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 flex items-center"
-                disabled={loading} 
-              >
-                {loading ? (
-                  <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
-                ) : (
-                  'Add Property'
-                )}
-              </button>
-            </div>
-          </form>
-        </div>
+          <div className="flex justify-end col-span-full">
+            <button
+              type="submit"
+              className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 flex items-center"
+              disabled={loading} 
+            >
+              {loading ? (
+                <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
+              ) : (
+                'Add Property'
+              )}
+            </button>
+          </div>
+        </form>
       </div>
-
     </div>
-  );
+  </div>
+);
 };
 
 export default LandlordAddProperty;
