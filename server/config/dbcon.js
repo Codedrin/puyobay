b10cf6153e5c0767f679def24d415cca043b977f
Codedrin <aldrinrosales428@gmail.com>
2024-11-24 19:03:46 +0800

    try

--- server/config/dbcon.js
+++ server/config/dbcon.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  mongoose.connect(process.env.MONGO_URI, {
+  mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://exhaustechteam77:xQv9ZgKYM9MyRFq0@puyobay.wkrwt.mongodb.net/PuyobayClient?retryWrites=true&w=majority&appName=Puyobay', {
     })
     .then(() => {
       console.log("Successfully connected to MongoDB Atlas!");
